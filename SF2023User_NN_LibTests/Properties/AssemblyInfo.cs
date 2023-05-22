a86f9fa10e72cb1c14bffa939f80337fd8235d7e
Динара Мурзабаева <murzabaeva-dd@mail.ru>
2023-05-22 18:14:39 +0400

    Добавьте файлы проекта.

---
+++ SF2023User_NN_LibTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("SF2023User_NN_LibTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SF2023User_NN_LibTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("93d9b66c-8d81-4535-adc0-f7a5d02a78c0")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SF2023User_NN_LibTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SF2023User_NN_Lib;
+using System;
+
+namespace SF2023User_NN_LibTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TimeSpan[] startTimes = new TimeSpan[]
+            {
+                new TimeSpan(10,0,0),
+                new TimeSpan(11,0,0),
+                new TimeSpan (15,0,0),
+                new TimeSpan (15,30,0),
+                new TimeSpan(16,50,0)
+            };
+            int[] durations = new int[] { 60, 30, 10, 10, 40 };
+            TimeSpan beginWorkingTime = new TimeSpan(8,0,0);
+            TimeSpan endWorkingTime= new TimeSpan(18,0,0);
+            int consultationTime = 30;
+
+            //test
+
+            string[] t = Calculations.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+            Assert.IsNotNull(t);
+        }
+    }
+}
